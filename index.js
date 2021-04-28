e304b4ec31ddddfd806e20a094fadb5e097aecfe
Oznnniv <>
2021-04-27 22:28:59 -0500

    Version for AWS

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3004;
-var mongoDB = 'mongodb://host.docker.internal:27017/acopios';
+//var mongoDB = 'mongodb://host.docker.internal:27017/acopios';
 //var mongoDB = 'mongodb://172.17.0.1:27017/acopios';
+var mongoDB = 'mongodb://database/acopios';
 
 mongoose.connect(mongoDB, {useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true}, (err, res) => {
 	if(err){
